61d7c873a778283b16d6fc935364d40b7426a305
Jason K. Moore <moorepants@gmail.com>
2022-02-15 07:15:51 +0100

    Wrap long mathjax output.

--- conf.py
+++ conf.py
@@ -44,9 +44,24 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Display TODO notes.
 todo_include_todos = True
+
+# This configures sphinx to number figures and allow referencing them, if
+# labeled, using :numref:`my_figure`.
 numfig = True
 
+# Sphinx >=4 default to MathJax v3, but v3 does not support wrapping lines. So
+# force Sphinx to use v2 and config MathJax to wrap long lines.
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+mathjax2_config = {
+    "HTML-CSS": {
+        "linebreaks": {"automatic": True}
+    }
+}
+
+# Setup intersphinx so that we can reference the SymPy documentation.
+# :external:py:func:`~sympy.physics.vector.functions.dot` for example.
 intersphinx_mapping = {
     'sympy': ('https://docs.sympy.org/latest/', None),
 }
@@ -57,7 +72,6 @@ intersphinx_mapping = {
 # a list of builtin themes.
 #
 html_theme = 'pydata_sphinx_theme'
-#html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
