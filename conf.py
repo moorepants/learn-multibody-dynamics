3a760a9a1e2c045f825ff1d84c5297f6a4d9c563
Jason K. Moore <moorepants@gmail.com>
2022-02-25 14:31:57 +0100

    Try piccolo theme.

--- conf.py
+++ conf.py
@@ -74,7 +74,7 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'piccolo_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
